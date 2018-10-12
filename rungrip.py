786bfdbfdce00a5264c0689e52be51685ea7cc53
n00shE <ryanblanchard00@gmail.com>
2018-10-12 22:30:12 +0000

    added resolution and fps sets

--- rungrip.py
+++ rungrip.py
@@ -29,36 +29,29 @@ def print_camera_information(cam):
 	print("Resolution: {0}, {1}.".format(round(cam.get_resolution().width, 2), cam.get_resolution().height))
 	print("Camera FPS: {0}.".format(cam.get_camera_fps()))
 	print("Firmware: {0}.".format(cam.get_camera_information().firmware_version))
-	print("Serial number: {0}.\n".format(cam.get_camera_information().serial_number))
+	print("Serial number: {0}.".format(cam.get_camera_information().serial_number))
 
 print("Running...")
+streamRunning = True
 init = zcam.PyInitParameters()
+init.camera_resolution = sl.PyRESOLUTION.PyRESOLUTION_HD720
+init.camera_fps = 15
 cam = zcam.PyZEDCamera()
+runtime = zcam.PyRuntimeParameters()
 if not cam.is_opened():
 	print("Opening ZED Camera...")
 status = cam.open(init)
 if status != tp.PyERROR_CODE.PySUCCESS:
 	print(repr(status))
+	streamRunning = False
 	exit()
+err = cam.grab(runtime)
+if err == tp.PyERROR_CODE.PySUCCESS:
+	streamRunning = True
 
-runtime = zcam.PyRuntimeParameters()
 mat = core.PyMat()
 
 print_camera_information(cam)
-
-key = ''
-while key != 113:  # for 'q' key
-    err = cam.grab(runtime)
-    if err == tp.PyERROR_CODE.PySUCCESS:
-        cam.retrieve_image(mat, sl.PyVIEW.PyVIEW_LEFT)
-        cv2.imshow("ZED", mat.get_data())
-        key = cv2.waitKey(5)
-    else:
-        key = cv2.waitKey(5)
-cv2.destroyAllWindows()
-
-cam.close()
-print("\nFINISH")
 
 now = datetime.datetime.now()
 print("Vision Log for " + str(now.day) + "/" + str(now.month) + "/" + str(now.year) + "  ~    " + str(now.hour) + ":" + str(now.minute) +":" + str(now.second))
@@ -67,7 +60,6 @@ print("Starting Vision...")
 
 bytes = ''
 version = int(cv2.__version__[:1])
-streamRunning = True
 pipeline = GripPipeline()
 
 try:
@@ -89,7 +81,7 @@ except:
 sd = NetworkTables.getTable('GRIP/myContoursReport')
 
 #cap = cv2.VideoCapture(0)
-
+"""
 try:
 	stream = urllib.urlopen("http://localhost:1180/?action=stream")
 except:
@@ -101,7 +93,7 @@ if streamRunning:
 else:
 	print("Stream OFFLINE")
 	print("FATAL ERROR")
-
+"""
 
 
 """
@@ -125,55 +117,58 @@ while(streamRunning == True):
 """
 
 while streamRunning:
-	bytes += stream.read(1024)
-	a = bytes.find('\xff\xd8')
-	b = bytes.find('\xff\xd9')
-	if a != -1 and b != -1:
-		jpg = bytes[a:b+2]
-		bytes = bytes[b+2:]
+	cam.retrieve_image(mat, sl.PyVIEW.PyVIEW_LEFT)
+	cv2.imshow("ZED", mat.get_data())
+	#bytes += stream.read(1024)
+	#a = bytes.find('\xff\xd8')
+	#b = bytes.find('\xff\xd9')
+	#if a != -1 and b != -1:
+		#jpg = bytes[a:b+2]
+		#bytes = bytes[b+2:]
 
-		color = cv2.CV_LOAD_IMAGE_COLOR if version == 2 else cv2.IMREAD_COLOR #name better
-		frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), color)
+		#color = cv2.CV_LOAD_IMAGE_COLOR if version == 2 else cv2.IMREAD_COLOR #name better
+		#frame = cv2.imdecode(np.fromstring(jpg, dtype=np.uint8), color)
 
-		cv2.imshow('Image', frame)
+		#cv2.imshow('Image', frame)
 
-		pipeline.process(frame)
+	pipeline.process(mat.get_data())
 		#print pipeline.boundingRects
 		#print pipeline.center
 		#print pipeline.filter_contours_output
 		#print pipeline.rects
 		#print pipeline.largestRect
-		if (pipeline.largestRect) != None: 
-			"""
-			#xtwo = pipeline.rects[0][0] + pipeline.rects[0][2]
-			#ytwo = pipeline.rects[0][1] + pipeline.rects[0][3]
-			#cv2.rectangle(frame, (pipeline.rects[0][0],pipeline.rects[0][1]), (xtwo,ytwo), (255,0,0), thickness=3, lineType=8, shift=0)
-			#cv2.imshow("Rectangle", frame)
-			"""
-			xtwo = pipeline.largestRect[0] + pipeline.largestRect[2]
-			ytwo = pipeline.largestRect[1] + pipeline.largestRect[3]
-		    
-			centerX = [pipeline.largestRect[0] + pipeline.largestRect[2]/2]
-			centerY = [pipeline.largestRect[1] + pipeline.largestRect[3]/2]
-			cv2.rectangle(frame, (pipeline.largestRect[0],pipeline.largestRect[1]), (xtwo,ytwo), (255,0,0), thickness=3, lineType=8, shift=0)
-			cv2.imshow("Rectangle", frame)
+	if (pipeline.largestRect) != None: 
+		"""
+		#xtwo = pipeline.rects[0][0] + pipeline.rects[0][2]
+		#ytwo = pipeline.rects[0][1] + pipeline.rects[0][3]
+		#cv2.rectangle(frame, (pipeline.rects[0][0],pipeline.rects[0][1]), (xtwo,ytwo), (255,0,0), thickness=3, lineType=8, shift=0)
+		#cv2.imshow("Rectangle", frame)
+		"""
+		xtwo = pipeline.largestRect[0] + pipeline.largestRect[2]
+		ytwo = pipeline.largestRect[1] + pipeline.largestRect[3]
+	    
+		centerX = [pipeline.largestRect[0] + pipeline.largestRect[2]/2]
+		centerY = [pipeline.largestRect[1] + pipeline.largestRect[3]/2]
+		cv2.rectangle(mat.get_data(), (pipeline.largestRect[0],pipeline.largestRect[1]), (xtwo,ytwo), (255,0,0), thickness=3, lineType=8, shift=0)
+		cv2.imshow("Rectangle", mat.get_data())
 
-			sd.putNumberArray("centerX", centerX)
-			sd.putNumberArray("centerY", centerY)
-			sd.putNumberArray("width", [pipeline.largestRect[2]])
-			sd.putNumberArray("height", [pipeline.largestRect[3]])
-			sd.putNumberArray("area", [pipeline.largestArea])
-#		    sd.putNumber("Test", len(pipeline.largestRect))
-#		    print pipeline.largestArea
-		else:
-			sd.putNumberArray("centerX", [])
-			sd.putNumberArray("centerY", [])
-			sd.putNumberArray("width", [])
-			sd.putNumberArray("height", [])
-			sd.putNumberArray("area", [])
+		sd.putNumberArray("centerX", centerX)
+		sd.putNumberArray("centerY", centerY)
+		sd.putNumberArray("width", [pipeline.largestRect[2]])
+		sd.putNumberArray("height", [pipeline.largestRect[3]])
+		sd.putNumberArray("area", [pipeline.largestArea])
+#		sd.putNumber("Test", len(pipeline.largestRect))
+#		print pipeline.largestArea
+	else:
+		sd.putNumberArray("centerX", [])
+		sd.putNumberArray("centerY", [])
+		sd.putNumberArray("width", [])
+		sd.putNumberArray("height", [])
+		sd.putNumberArray("area", [])
 
 
 		# Press Q on keyboard to  exit
-		#if cv2.waitKey(25) & 0xFF == ord('q'):
-			#break
+	if cv2.waitKey(25) & 0xFF == ord('q'):
+		cam.close()
+		break
    
